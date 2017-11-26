09ed2112c35bcf3160c1cfab00e02a3fd1c0aac8
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2017-11-26 14:08:10 +0600

    Look up weather by city and country name instead of id

--- index.js
+++ index.js
@@ -5,26 +5,18 @@ const axios = require('axios');
 const qs = require('querystring');
 const url = require('url');
 
-const CITYLIST = require('./city.list.json');
 const ENDPOINT = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = process.env.API_KEY;
 
 module.exports = async (req, res) => {
   let queryParams = qs.parse(url.parse(req.url).query);
-  let cityId = null;
 
-  if (!queryParams.country) {
-    return micro.send(res, 400, 'Country code is required!');
-  }
-
-  if (queryParams.country && queryParams.city) {
-    cityId = CITYLIST.find(city => (city.name === queryParams.city || city.name.includes(queryParams.city)) && city.country === queryParams.country).id;
-  } else { // find closest match if only country is provided
-    cityId = CITYLIST.find(city => city.country === queryParams.country).id;
+  if (!queryParams.country || !queryParams.city) {
+    return micro.send(res, 400, 'Country and/or City is missing!');
   }
 
   try {
-    let results = await axios.get(`${ENDPOINT}?id=${cityId}&appid=${API_KEY}&units=metric`);
+    let results = await axios.get(`${ENDPOINT}?q=${queryParams.city},${queryParams.country}&appid=${API_KEY}&units=metric`);
     res.end(JSON.stringify(Object.assign(results.data.main, { condition: results.data.weather[0].main })));
   } catch(e) {
     micro.send(res, 400, e.message || e);
